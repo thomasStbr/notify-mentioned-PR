731dea1938f534e8c255e5e4f38bc65e4d3c2e40
thomasStbr <thomas.stauber@protonmail.com>
2023-12-02 16:37:24 +0100

    test

--- actions/mention-check/mention-check.js
+++ actions/mention-check/mention-check.js
@@ -1,14 +1,17 @@
 const github = require('@actions/github');
+const core = require('@actions/core');
 
 async function run() {
+    const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+    const octokit = github.getOctokit(GITHUB_TOKEN);
+    console.log(octokit);
+    console.log(process.env.GITHUB_TOKEN);
 
-    const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
-    print(octokit);
-    print(process.env.GITHUB_TOKEN);
+    const pullReq = context.payload.pull_request;
+    const pullRequestNumber = pullReq.number;
 
-    const pullRequestNumber = context.payload.pull_request.number;
+    console.log(pullReq);
 
-    print(pullRequest);
 
     const { data: pullRequest } = await octokit.rest.pulls.get({
         owner: context.repo.owner,
