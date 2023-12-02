a81fc5e106975290ac4e4c7fc87b571f6c44d77f
thomasStbr <thomas.stauber@protonmail.com>
2023-12-02 18:02:19 +0100

    test with local pr

--- actions/mention-check/mention-check.js
+++ actions/mention-check/mention-check.js
@@ -10,9 +10,27 @@ async function run() {
 
     const repository = 'thomasStbr/notify-mentioned-PR';
 
-    listRemotePR(octokit, "thomasStbr", "notify-mentioned-PR");
 
-    listRemotePR(octokit, owner, repo, false);
+    const openPullRequests = listRemotePR(octokit, owner, repo, false);
+
+    openPullRequests.forEach((pr) => {
+        const { mentionedPRNumber, mentionedPROwner, mentionedPRRepo } = extractMentionedPRInfo(pr.body);
+
+        console.log(`Mentioned PR Number: ${mentionedPRNumber}`);
+        console.log(`Mentioned PR Owner: ${mentionedPROwner}`);
+        console.log(`Mentioned PR Repository: ${mentionedPRRepo}`);
+
+        console.log("PR is closed ? ", isPRClosed(octokit, mentionedPROwner, mentionedPRRepo, mentionedPRNumber));
+
+
+
+    });
+
+
+
+    //listRemotePR(octokit, "thomasStbr", "notify-mentioned-PR");
+
+
 
 }
 async function listRemotePR(octokit, repoOwner, repoName, closed = true) {
@@ -29,9 +47,45 @@ async function listRemotePR(octokit, repoOwner, repoName, closed = true) {
         console.log(`  Description: ${pr.body}`);
         console.log('---');
     });
+
+
+
+    return pullRequests;
+}
+
+async function isPRClosed(octokit, repoOwner, repoName, prNumber) {
+    try {
+        // Get information about the specific pull request
+        const { data: pullRequest } = await octokit.pulls.get({
+            owner: repoOwner,
+            repo: repoName,
+            pull_number: prNumber,
+        });
+
+        // Check if the pull request state is "closed"
+        return pullRequest.state === 'closed';
+    } catch (error) {
+        console.error(`Error checking the state of PR #${prNumber}:`, error.message);
+        throw error;
+    }
 }
 
 
+function extractMentionedPRInfo(body) {
+    // Regular expression to match the mentioned PR information
+    const regex = /Mentioned PR: #(\d+) \((https:\/\/github\.com\/([^\/]+)\/([^\/]+))\)/;
+    const match = body.match(regex);
+
+    if (match) {
+        const mentionedPRNumber = match[1];
+        const mentionedPROwner = match[3];
+        const mentionedPRRepo = match[4];
+
+        return { mentionedPRNumber, mentionedPROwner, mentionedPRRepo };
+    }
+
+    return {};
+}
 
 run().catch(error => {
     console.error(error);
